ad2a19922b2d8a9a55c4cb3c026fbe71799b2dff
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-04 19:12:24 +0530

    Initial

---
+++ linecomp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace linecomp2
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("... Welcome to Line Comparison Computation ... ");
+
+            checkequlityline Linecom = new checkequlityline();
+            Linecom.length_line();
+            Linecom.Comparelines();
+            Console.ReadKey();
+
+        }
+    }
+}
